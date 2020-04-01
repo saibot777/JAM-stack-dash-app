655983f1b918b2410ea99b4e05caafd8f4c9e5ef
saibot777 <stefantrajkovic777@gmail.com>
2020-04-01 11:49:19 +0200

    rendering my repos

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const listRepos = async username => {
   try {
-    let repos = await fetch(
+    const repos = await fetch(
       `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
-    );
-    repos = repos.json();
+    ).then(res => res.json());
+
+    console.log(repos, "REPOSSS");
+
+    const markup = repos
+      .map(
+        repo => `
+            <li>
+                <a href="${repo.html_url}">${repo.name}</a>
+                (${repo.stargazers_count})
+            </li>
+        `
+      )
+      .join("");
+
+    const content = document.getElementById("content");
+
+    content.innerHTML = `<ul>${markup}</ul`;
   } catch (error) {
     console.log(error);
     throw error;
   }
 };
+
+listRepos("saibot777");
