3da6cd9c1f7e38f989afa50d8e0242e62fa36c2d
saibot777 <stefantrajkovic777@gmail.com>
2020-04-08 11:53:38 +0200

    main index impl

---
+++ dashboard/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => (
+  <>
+    <h1>This App rules!</h1>
+    <p>Log in to find out why.</p>
+    <Link to="/dashboard">Go to the dashboard</Link>
+  </>
+);
