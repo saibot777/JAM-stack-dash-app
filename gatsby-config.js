75dda96250fb4f591222a2903ca6bcc5df5f34fd
saibot777 <stefantrajkovic777@gmail.com>
2020-04-08 13:38:01 +0200

    gatsby config upt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: `Dashboard`,
+    siteUrl: "https://cranky-davinci-1a32a2.netlify.com/",
+  },
+  plugins: [],
+};
+
 exports.onCreatePage = async ({ page, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
 
